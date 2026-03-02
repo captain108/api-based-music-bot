ea0f162242df85d8ffc3f26f3e46b8b47042245b
Captain <papajiop11@gmail.com>
2026-03-02 21:18:15 +0530

    Update queue.py

--- queue.py
+++ queue.py
@@ -2,16 +2,16 @@ from collections import defaultdict
 
 queues = defaultdict(list)
 
-def add_to_queue(chat_id, song):
+def add_song(chat_id, song):
     queues[chat_id].append(song)
 
-def get_next(chat_id):
+def next_song(chat_id):
     if queues[chat_id]:
         return queues[chat_id].pop(0)
     return None
 
-def clear_queue(chat_id):
-    queues[chat_id].clear()
-
 def get_queue(chat_id):
     return queues[chat_id]
+
+def clear_queue(chat_id):
+    queues[chat_id].clear()
