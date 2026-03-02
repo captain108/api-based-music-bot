49632789948f2fcb8a91cf7d759303349755982f
Captain <papajiop11@gmail.com>
2026-03-02 21:17:33 +0530

    Load configuration from environment variables
    Refactor config.py to load environment variables for configuration.

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
-API_ID=
-API_HASH=
-BOT_TOKEN=
-STRING_SESSION=
-YTDLP_API=
-YTDLP_KEY=
+import os
 
-BOT_NAME=Captain Music
-OWNER_NAME=Captain
-OWNER_USERNAME=captainpapaj1
-CHANNEL_USERNAME=yourchannel
-SUPPORT_USERNAME=yoursupport
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+STRING_SESSION = os.getenv("STRING_SESSION")
+
+YTDLP_API = os.getenv("YTDLP_API")
+YTDLP_KEY = os.getenv("YTDLP_KEY")
+
+BOT_NAME = os.getenv("BOT_NAME")
+OWNER_NAME = os.getenv("OWNER_NAME")
+OWNER_USERNAME = os.getenv("OWNER_USERNAME")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
+SUPPORT_USERNAME = os.getenv("SUPPORT_USERNAME")
