149146eb04386242bb54cbb85ff77be771d3be94
Captain <papajiop11@gmail.com>
2026-03-02 22:49:47 +0530

    Update player.py

--- player.py
+++ player.py
@@ -1,11 +1,7 @@
-from pytgcalls.types.input_stream import InputStream, AudioPiped
-from pytgcalls.types.input_stream.quality import HighQualityAudio
-from queue import next_song
+from pytgcalls.types.input_stream import AudioPiped
 
 async def play(call, chat_id, song):
     await call.join_group_call(
         chat_id,
-        InputStream(
-            AudioPiped(song["audio_url"], HighQualityAudio())
-        )
+        AudioPiped(song["audio_url"])
     )
